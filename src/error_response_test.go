33301728bd6f49812af8b3dc891e2f57d89c2b4e
Swackles <Swackles@users.noreply.github.com>
2020-09-16 21:45:01 +0300

    refactored error_response_test.go

--- src/error_response_test.go
+++ src/error_response_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 )
 
-// Test respones
 var OKResponse = &http.Response{
 	StatusCode: 200,
 }
@@ -14,79 +13,36 @@ var NotOKResponse = &http.Response{
 	StatusCode: 500,
 }
 
-// Test bodies to be used
-var ErrorCodeString = []byte("{ \"code\": \"UserRequired\", \"message\": \"Only a logged user is authorized to perform this operation\" }")
-var ErrorIDString = []byte("{ \"id\": -3, \"message\": \"Service not found: lastactivities.getlastactivities(1)\" }")
-var ErrorContainerIDString = []byte("{ \"error\": { \"id\": 334, \"message\": \"Malformed email\" }}")
+var errorCodeString = []byte("{ \"code\": \"code\", \"message\": \"msg\" }")
+var errorCodeStringErrorResponse = &ErrorResponse{Code: "code", Message: "msg", StatusCode: 500}
 
-// Test when TestParseError should return ErrorResponse
-func TestParseErrorReturnErrorResponse(t *testing.T) {
-	// response.status == 200 && Error message contains ID
-	result := parseError(OKResponse, ErrorContainerIDString)
-	expected := &ErrorResponse{
-		ID:         334,
-		Message:    "Malformed email",
-		StatusCode: 200,
-	}
-	testParseErrorPrint(testParseErrorStruct{
-		success:  reflect.DeepEqual(result, expected),
-		input:    []string{"OKResponse", "ErrorString"},
-		expected: expected,
-		got:      result,
-	}, t)
+var errorIDString = []byte("{ \"id\": 3, \"message\": \"msg\" }")
+var errorIDStringErrorResponse = &ErrorResponse{ID: 3, Message: "msg", StatusCode: 500}
 
-	// response.status != 200 && Error message contains code
-	result = parseError(NotOKResponse, ErrorCodeString)
-	expected = &ErrorResponse{
-		Code:       "UserRequired",
-		Message:    "Only a logged user is authorized to perform this operation",
-		StatusCode: 500,
+var errorContainerIDString = []byte("{ \"error\": { \"id\": 1, \"message\": \"msg\" }}")
+var errorContainerIDStringErrorResponse = &ErrorResponse{ID: 1, Message: "msg", StatusCode: 200}
+
+func TestParseError(t *testing.T) {
+	var result *ErrorResponse
+
+	result = parseError(OKResponse, errorContainerIDString)
+	if !reflect.DeepEqual(result, errorContainerIDStringErrorResponse) {
+		t.Errorf("when received error with 200 statuscode, should return ErrorResponse")
 	}
-	testParseErrorPrint(testParseErrorStruct{
-		success:  reflect.DeepEqual(result, expected),
-		input:    []string{"NotOKResponse", "ErrorString"},
-		expected: expected,
-		got:      result,
-	}, t)
+
+	result = parseError(NotOKResponse, errorCodeString)
+	if !reflect.DeepEqual(result, errorCodeStringErrorResponse) {
+		t.Errorf("when received error code with 200 statuscode, should return ErrorResponse")
+	}
 
 	// response.status != 200 && Error message contains ID
-	result = parseError(NotOKResponse, ErrorIDString)
-	expected = &ErrorResponse{
-		ID:         -3,
-		Message:    "Service not found: lastactivities.getlastactivities(1)",
-		StatusCode: 500,
+	result = parseError(NotOKResponse, errorIDString)
+	if !reflect.DeepEqual(result, errorIDStringErrorResponse) {
+		t.Errorf("when received error id with 200 statuscode, should return ErrorResponse")
 	}
-	testParseErrorPrint(testParseErrorStruct{
-		success:  reflect.DeepEqual(result, expected),
-		input:    []string{"NotOKResponse", "ErrorString"},
-		expected: expected,
-		got:      result,
-	}, t)
-}
 
-// Test when parseError should return nil
-func TestParseErrorReturnNil(t *testing.T) {
-	result := parseError(OKResponse, []byte("{ \"status\": \"OK\" }"))
-	testParseErrorPrint(testParseErrorStruct{
-		success:  result == nil,
-		input:    []string{"OKResponse", "{ \"status\": \"OK\" }"},
-		expected: nil,
-		got:      result,
-	}, t)
-}
-
-type testParseErrorStruct struct {
-	success  bool
-	input    []string
-	expected *ErrorResponse
-	got      *ErrorResponse
-}
-
-func testParseErrorPrint(input testParseErrorStruct, t *testing.T) {
-	method := "parseError(" + input.input[0] + ", " + input.input[1] + ")"
-	if input.success {
-		t.Logf("%+v success,\n    should return %+v", method, input.expected)
-	} else {
-		t.Errorf("%+v failed\n    expected: %+v\n         got: %+v", method, input.expected, input.got)
+	result = parseError(OKResponse, []byte("{ \"status\": \"OK\" }"))
+	if result != nil {
+		t.Errorf("when received correct response with 200 statuscode, should return nil")
 	}
 }
