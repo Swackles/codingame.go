f1daafe68463303274b79fa1a8f0e73d6ee79d44
Swackles <svenerik.rebane@gmail.com>
2020-09-18 15:28:16 +0300

    moved action_types -> login_response

--- src/login_response.go
+++ src/login_response.go
@@ -53,9 +53,43 @@ type User struct {
 	Properties Properties `json:"properties"`
 }
 
-func newLoginResponse(body []byte, client CodinGameClient) *LoginResponse {
-	var response *LoginResponse
-	json.Unmarshal(body, &response)
+type ActionTypes struct {
+	DeleteContribution                   ActionType `json:"deleteContribution"`
+	ReceiveCareerNotification            ActionType `json:"receiveCareerNotification"`
+	DeleteComment                        ActionType `json:"deleteComment"`
+	SubmitMultiContribution              ActionType `json:"submitMultiContribution"`
+	SendPingEmailToAll                   ActionType `json:"sendPingEmailToAll"`
+	AccessCoursesBeta                    ActionType `json:"accessCoursesBeta"`
+	EditAcceptedContribution             ActionType `json:"editAcceptedContribution"`
+	UpdateAvatar                         ActionType `json:"updateAvatar"`
+	UpdateAllAvatar                      ActionType `json:"updateAllAvatar"`
+	AccessDisabledLeaderboard            ActionType `json:"accessDisabledLeaderboard"`
+	Impersonate                          ActionType `json:"impersonate"`
+	EditComment                          ActionType `json:"editComment"`
+	EditContribution                     ActionType `json:"editContribution"`
+	DenyPuzzleContribution               ActionType `json:"denyPuzzleContribution"`
+	SendPingEmailToOptinCodingamers      ActionType `json:"sendPingEmailToOptinCodingamers"`
+	DenyClashContribution                ActionType `json:"denyClashContribution"`
+	ValidateClashContribution            ActionType `json:"validateClashContribution"`
+	SendPrivateMessageToAll              ActionType `json:"sendPrivateMessageToAll"`
+	DenyContribution                     ActionType `json:"denyContribution"`
+	EditClashContribution                ActionType `json:"editClashContribution"`
+	UpdateCover                          ActionType `json:"updateCover"`
+	EditPuzzleContribution               ActionType `json:"editPuzzleContribution"`
+	SendPrivateMessageToOptinCodingamers ActionType `json:"sendPrivateMessageToOptinCodingamers"`
+	EditAcceptedPuzzleContribution       ActionType `json:"editAcceptedPuzzleContribution"`
+	UpdateCareerSettings                 ActionType `json:"updateCareerSettings"`
+	EditAcceptedClashContribution        ActionType `json:"editAcceptedClashContribution"`
+	ValidateContribution                 ActionType `json:"validateContribution"`
+	ValidatePuzzleContribution           ActionType `json:"validatePuzzleContribution"`
+	UpdateAllCover                       ActionType `json:"updateAllCover"`
+}
+
+type ActionType struct {
+	AuthorPolicy       string `json:"authorPolicy"`
+	MinCodinGamerCount int    `json:"minCodinGamerCount"`
+}
+
 
 	client.CodinGamerID = response.Success.CodinGamer.UserID
 	response.Success.CodinGamer.Client = client
